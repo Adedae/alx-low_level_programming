5572ea6d062482b4cd5867ae342151a778ba0ee1
Adedae <getontowycliffe@gmail.com>
2023-09-09 14:14:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to a error
+ * main - Entry point
  * Return 1 (Success)
  */
 int main(void)
 {
-	("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,
+	"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
