6746383be4bbd594b8fb599d86b74d408baaef97
Adedae <getontowycliffe@gmail.com>
2023-10-02 08:11:33 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 /**
- * create_array - a function that creates an array of chars, and initialize c
+ * create_array - a function that creates an array of chars, and assigns char c
  * @size: size of array
  * @c: char to assign
- * Description: create array of size and assign char c
+ * Description: create array of size size  and assign char c
  * Return: pointer to array, NULL if fail
  *
  */
 char *create_array(unsigned int size, char c)
 {
-	char *strr;
+	char *str;
 	unsigned int i;
 
 	str = malloc(sizeof(char) * size);
