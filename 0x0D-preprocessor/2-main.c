55ab744bee308b5fb52423cd878028967ca3fc60
Adedae <getontowycliffe@gmail.com>
2023-10-09 11:43:07 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,11 @@
 
 /**
  * main-  a program that prints the name of the file it was compiled from
- *
- * Return: Always 0 (Success)
+ *Return: Always 0 (Success)
  */
 int main(void)
-{       
-        printf("%s\n",__FILE__);
+{
+	printf("%s\n", __FILE__);
 	return (0);
 }
+
